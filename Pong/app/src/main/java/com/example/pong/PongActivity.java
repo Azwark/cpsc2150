2c01d8f1072c42566918fb5c64927dd1bdac7341
Azwark <30425457+Azwark@users.noreply.github.com>
2021-05-21 14:36:53 -0400

    Add files via upload

---
+++ Pong/app/src/main/java/com/example/pong/PongActivity.java
@@ -0,0 +1,17 @@
+package com.example.pong;
+
+import android.app.Activity;
+import android.view.Window;
+import android.os.Bundle;
+
+public class PongActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+    }
+}
